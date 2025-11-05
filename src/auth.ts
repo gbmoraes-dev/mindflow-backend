ed11c5462f401847ff488f105b2154bdc6c4d539
gbmoraes-dev <dev.gbmoraes@gmail.com>
2025-11-05 11:09:55 -0300

    feat: add better auth config

---
+++ src/auth.ts
@@ -0,0 +1,42 @@
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { openAPI } from 'better-auth/plugins'
+import { db } from './db'
+import { env } from './env'
+
+export const auth = betterAuth({
+  basePath: '/auth',
+  plugins: [openAPI()],
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    usePlural: true,
+  }),
+  advanced: {
+    generateId: false,
+  },
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+    password: {
+      hash: (password: string) => Bun.password.hash(password),
+      verify: ({ password, hash }) => Bun.password.verify(password, hash),
+    },
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 5, // 5 minutes
+    },
+  },
+})
