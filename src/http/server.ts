5ac91f328c85bc8551b736b24f6ad1e1937dba66
gbmoraes-dev <dev.gbmoraes@gmail.com>
2025-11-07 18:45:41 -0300

    feat: add global error handler and private routes to server

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,15 +3,19 @@ import { env } from '@/env'
 import { tracing } from '@/tracing'
 import { betterAuthPlugin } from './plugins/better-auth'
 import { corsPlugin } from './plugins/cors'
+import { errorHandler } from './plugins/error-handler'
 import { openapiPlugin } from './plugins/openapi'
+import { privateRoutes } from './routes/private'
 import { publicRoutes } from './routes/public'
 
 const app = new Elysia()
   .use(tracing)
   .use(corsPlugin)
   .use(openapiPlugin)
+  .use(errorHandler)
   .use(betterAuthPlugin)
   .use(publicRoutes)
+  .use(privateRoutes)
   .listen({ hostname: env.HOST, port: env.PORT })
 
 console.log(
