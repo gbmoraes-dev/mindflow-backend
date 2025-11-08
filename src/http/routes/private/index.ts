978d5bf1bf521e768832da036a4b29dd97e81898
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-11-08 01:00:37 -0300

    feat: add websocket updates route

--- src/http/routes/private/index.ts
+++ src/http/routes/private/index.ts
@@ -3,9 +3,11 @@ import { createJournal } from './create-journal'
 import { getJournal } from './get-journal'
 import { getSummary } from './get-summary'
 import { listJournals } from './list-journals'
+import { websocket } from './websocket'
 
 export const privateRoutes = new Elysia()
   .use(createJournal)
   .use(getJournal)
   .use(listJournals)
   .use(getSummary)
+  .use(websocket)
---
+++ src/http/routes/private/websocket.ts
@@ -0,0 +1,37 @@
+import { Elysia } from 'elysia'
+import { RedisClient } from 'bun'
+import { z } from 'zod/v4'
+import { env } from '@/env'
+
+export const websocket = new Elysia()
+  .state('subscriber', undefined as RedisClient | undefined)
+  .ws('/ws/journal/:journalId', {
+    params: z.object({
+      journalId: z.string()
+    }),
+
+    async open(ws) {
+      const { journalId } = ws.data.params
+
+      const channel = `journal:${journalId}:completed`
+
+      const subscriber = new RedisClient(env.REDIS_URL)
+
+      await subscriber.subscribe(channel, (message) => {
+        ws.send(message)
+      })
+
+      ws.data.store.subscriber = subscriber
+
+      ws.send(JSON.stringify({
+        type: 'connected',
+        journalId
+      }))
+    },
+
+    async close(ws) {
+      if (ws.data.store.subscriber) {
+        await ws.data.store.subscriber.unsubscribe()
+      }
+    }
+  })
